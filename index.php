1eee3eaf569e693bde22440aab41de01da5ec5a1
leechunhang <cyrus@splashinteractive.com.hk>
2015-07-29 23:32:37 +0800

    test
    test

---
+++ index.php
@@ -0,0 +1,147 @@
+<>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+    <head>
+
+        <title>Prestige</title>
+		<meta name="fragment" content="!"/>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+
+        <link rel="stylesheet" type="text/css" href="jquery.qtip.css"/>
+        <link rel="stylesheet" type="text/css" href="nivo-slider.css"/> 
+        <link rel="stylesheet" type="text/css" href="jquery.jScrollPane.css"/>
+        <link rel="stylesheet" type="text/css" href="jquery.fancybox.css"/> 
+
+        <link rel="stylesheet" type="text/css" href="base.css"/> 
+        <link rel="stylesheet" type="text/css" href="page.css"/> 
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700,300,200"/>    
+
+    </head>
+
+    <body>
+        
+        <!-- Main -->
+        <div class="main box-center">
+            
+            <!-- Header -->
+            <div class="header layout-50 overflow-fix">
+                
+                <!-- Logo -->
+                <div class="layout-50-left">
+                    <h1>Ruth Howell</h1>
+                    <h4>Creative Designer &amp; Developer</h4>
+                </div>
+                <!-- /Logo -->
+                
+                <!-- Latest tweets -->
+                <div class="layout-50-right">
+                    <div id="latest-tweets"></div>
+                </div>
+                <!-- /Latest tweets -->
+                
+            </div>
+            <!-- /Header -->            
+            
+            <!-- Content -->
+            <div class="content">
+                
+                <div id="prestige">
+                    
+                    <!-- Previous arrow -->
+                    <a href="#" id="prestige-navigation-prev"></a>
+                    <!-- /Previous arrow -->
+
+                    <!-- Menu -->
+                    <ul id="prestige-menu">
+                        <li id="prestige-menu-element-page-1">
+                            <a href="#!/profile">
+                                <span class="title">Profile</span>
+                                <span class="subtitle">About me</span>
+                            </a>
+                        </li>
+                        <li id="prestige-menu-element-page-2">
+                            <a href="#!/resume">
+                                <span class="title">Resume</span>
+                                <span class="subtitle">Personal info</span>
+                            </a>
+                        </li>
+                        <li id="prestige-menu-element-page-3">
+                            <a href="#!/folio">
+                                <span class="title">Folio</span>
+                                <span class="subtitle">My works</span>
+                            </a>
+                        </li>
+                        <li id="prestige-menu-element-page-4">
+                            <a href="#!/references">
+                                <span class="title">References</span>
+                                <span class="subtitle">Work experience</span>
+                            </a>
+                        </li>
+                        <li id="prestige-menu-element-page-5">
+                            <a href="#!/contact">
+                                <span class="title">Contact</span>
+                                <span class="subtitle">Get in touch</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- /Menu -->
+                    
+                    <!-- Window -->
+                    <div id="prestige-window"> 
+                        
+                        <!-- Background -->
+                        <div id="prestige-window-background">
+                            
+                            <!-- Scroll area -->
+                            <div id="prestige-window-scroll">
+                    
+                                <!-- Content -->
+                                <div id="prestige-window-content">
+
+                                </div>
+                                <!-- /Content -->
+                                
+                            </div>
+                            <!-- /Scroll area -->
+
+                        </div>
+                        <!-- /Background -->
+                        
+                    </div>
+                    <!-- /Window -->
+                    
+                    <!-- Next arrow -->
+                    <a href="#" id="prestige-navigation-next"></a>
+                    <!-- /Next arrow -->
+
+                </div>
+                
+            </div>
+            <!-- /Content -->
+            
+            <!-- Footer -->
+            <div class="footer">
+                
+                <!-- Menu -->
+                <ul class="footer-menu no-list">
+                    <li>Copyright 2012 Ruth Howell</li>
+                    <li><a href="#!/profile">Profile</a></li>
+                    <li><a href="#!/resume">Resume</a></li>
+                    <li><a href="#!/folio">Folio</a></li>
+                    <li><a href="#!/references">References</a></li>
+                    <li><a href="#!/contact">Contact</a></li>
+                </ul>
+                <!-- /Menu -->
+                
+            </div>
+            <!-- /Footer -->
+            
+        </div>
+        <!-- /Main -->
+        
+    </body>
+
+</html>
